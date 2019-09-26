460f953f9d6b7acf0918af7aa5215eaa0c718a94
ludoviclambinon <ludoviclambinon.work@gmail.com>
2019-09-26 12:24:12 +0200

    upd video 17 end

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@
 */
 
 Route::view('/', 'welcome');
-Route::view('contact', 'contact');
 Route::view('a-propos', 'a-propos');
 
 //Clients
+/*
 Route::get('clients', 'ClientsController@index'); //Avant, ClientsController@list car nom de la fonction
 Route::get('clients/create', 'ClientsController@create');
 Route::post('clients', 'ClientsController@store');
@@ -23,3 +23,10 @@ Route::get('clients/{client}', 'ClientsController@show');
 Route::get('clients/{client}/edit', 'ClientsController@edit');
 Route::patch('clients/{client}', 'ClientsController@update');
 Route::delete('clients/{client}', 'ClientsController@destroy');
+*/
+//Toutes les routes sont reprises dans la ligne en dessous
+Route::resource('clients', 'ClientsController');
+
+//Contact
+Route::get('contact', 'ContactController@create');
+Route::post('contact', 'ContactController@store');
