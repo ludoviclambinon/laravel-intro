50ce7638deb9d95c8b75105bd2ee5e2a7ca2f413
ludoviclambinon <ludoviclambinon.work@gmail.com>
2019-10-03 15:24:01 +0200

    end video 23

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Events\UserHasRegisteredEvent;
 
 class RegisterController extends Controller
 {
@@ -63,10 +64,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        //Evénements
+        event(new UserHasRegisteredEvent($user));
+
+        return $user;
     }
 }
+
+//Lorsqu'un nouveau user est créé, un mail est envoyé à celui-ci en reprenant le modèle user
